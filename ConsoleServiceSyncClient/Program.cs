25e9fcf81b6151d7dd9b2c0f6c9b7ccbd34da85f
Jim Mittler <jim.mittler@xtime.com>
2015-06-04 11:24:18 -0700

    initial commit

---
+++ ConsoleServiceSyncClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleServiceSyncClient.DMSUtility;
+
+namespace ConsoleServiceSyncClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DMSUtilityClient client = new DMSUtilityClient();
+            Response r = client.DmsPing();
+            Console.WriteLine(r);
+        }
+    }
+}
